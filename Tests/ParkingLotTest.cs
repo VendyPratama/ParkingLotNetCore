4b8b8e998416f67d00ea18784e39179ae3647595
Vendy <dwivendypratama@gmail.com>
2019-06-03 12:05:32 +0700

    Updating directory, added Test project using xunit

---
+++ Tests/ParkingLotTest.cs
@@ -0,0 +1,21 @@
+using Models;
+using Xunit;
+
+namespace Tests {
+    public class ParkingLotTest {
+
+        [Fact]
+        public void TestName () {
+            string parkingLotName = "Test Park";
+            //Given
+            var instanceParkingLot1 = ParkingLot.Instance (parkingLotName);
+
+            //When
+            var instanceParkingLot2 = ParkingLot.Instance ("Test2 Park");
+
+            //Then
+            Assert.Equal(expected: instanceParkingLot1, actual: instanceParkingLot2);
+            Assert.Equal(expected: parkingLotName, actual: instanceParkingLot2.ToString());
+        }
+    }
+}
